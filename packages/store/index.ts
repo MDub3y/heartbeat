53d5fccb394a88ca7225afce0ae1bb83e487e719
manasvi001 <manasvi.d007@gmail.com>
2026-02-18 10:13:58 +0530

    postinstall fix

--- packages/store/index.ts
+++ packages/store/index.ts
@@ -7,5 +7,3 @@ export const prismaClient = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prismaClient;
 }
-
-export * from "./model";
