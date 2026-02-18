a45bb561a9d3e2e504b46bb81f85c809c0fa3f20
manasvi001 <manasvi.d007@gmail.com>
2026-02-18 18:37:55 +0530

    added prisma export

--- packages/store/index.ts
+++ packages/store/index.ts
@@ -7,3 +7,5 @@ export const prismaClient = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prismaClient;
 }
+
+export * from "./generated/prisma";
