786c18357d571ff9fa6d680adea15cb47a03ec31
manasvi001 <manasvi.d007@gmail.com>
2026-02-18 19:33:02 +0530

    added deployed url to cors

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(helmet());
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://uptime-jls0pgxsy-manasvis-projects-641394dc.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"]
