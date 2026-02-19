a30901fb74069a4f6a86fee39869e5d6e03c39b1
manasvi001 <manasvi.d007@gmail.com>
2026-02-19 22:31:05 +0530

    fixed utils

--- apps/api/utils.ts
+++ apps/api/utils.ts
@@ -1,6 +1,9 @@
 import type { Request, Response, NextFunction } from "express";
 
-export const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
+export const asyncHandler =
+  <T extends Request>(
+    fn: (req: T, res: Response, next: NextFunction) => Promise<any>
+  ) =>
+    (req: T, res: Response, next: NextFunction) => {
+      Promise.resolve(fn(req, res, next)).catch(next);
+    };
